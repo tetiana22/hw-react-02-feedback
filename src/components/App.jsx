cf9d00a9e1e22c1ceac817470e378b9597030230
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2023-12-12 20:56:02 +0000

    create feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { Section } from "./Section/Section";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { Statistics } from "./Statistics/Statistics";
+import { Container } from "./App.styled";
+
+ export class App extends Component {
+  state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+
+  }
+  onLeaveFeedback = state => {
+    this.setState(prevState => ({ [state]: prevState[state] + 1 }))
+  }
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total > 0 ? ((good / total) * 100).toFixed(0) : 0;
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+    const totalFeedback = this.countTotalFeedback();
+    const totalPercentage = this.countPositiveFeedbackPercentage(); 
+
+    return (
+      <Container>
+         <Section title="Please leave feedback">
+           <FeedbackOptions
+              onLeaveFeedback={this.onLeaveFeedback}
+              options={options}
+           />
+         </Section>
+         <Section title="Statistics">
+           <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={totalPercentage}
+           />
+        </Section>
+      </Container>
+    )
+  }
+  
 };
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  font-size: 22px;
+  padding-left: 5px;
+`;
